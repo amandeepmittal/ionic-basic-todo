61992d9c40d90b860506daa56c736634b458679a
amandeepmittal <amandeepmittal@live.com>
2017-06-16 18:26:57 +0530

    setup HomePage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,8 +6,25 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public items;
 
   constructor(public navCtrl: NavController) {
+
+  }
+
+  ionViewDidLoad() {
+    this.items = [
+      {title: 'Item 1', description: 'Description 1'},
+      {title: 'Item 2', description: 'Description 2'},
+      {title: 'Item 3', description: 'Description 3'},
+    ]
+  }
+
+  addItem() {
+
+  }
+
+  viewItem() {
 
   }
 
